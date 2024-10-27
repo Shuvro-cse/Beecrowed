a5c6fe40e5b8810f6d2683c553453df3f4568346
Shuvro-cse <shuvrosarker.inbox@gmail.com>
2024-10-27 17:30:51 +0600

    Add files via upload

---
+++ 1145.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int x,y,i,j,k,l,m=1;
+    scanf("%d %d",&x,&y);
+    k=y/x;
+    l=x;
+    for(i=1;i<=k;i++)
+    {
+        printf("%d",m);
+        for(j=m+1;j<=l;j++)
+            printf(" %d",j);
+        printf("\n");
+        m=m+l;
+
+    }
+    return 0;
+}
