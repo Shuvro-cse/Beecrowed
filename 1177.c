7ad9e2f7ecfbb0960c6d05a39bb93c22095590b0
Shuvro-cse <shuvrosarker.inbox@gmail.com>
2024-11-06 10:09:22 +0600

    Add files via upload

---
+++ 1177.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int T,i,j,N[1000];
+    scanf("%d",&T);
+    for(i=0,j=0;i<1000;i++)
+    {
+        printf("N[%d] = %d\n",i,j);
+        j++;
+        if(j==T)
+        {
+            j=0;
+        }
+    }
+    return 0;
+}
